c8dde534432b72cce8d0247ec967781a4da7c324
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-24 18:58:53 +0000

    generator.js added markdown for the readme

--- generator.js
+++ generator.js
@@ -1,6 +1,41 @@
-const generateReadme = (data) => {
+const generateReadme = (input) => {
     return `
-    
+    # ${input.title}
+
+    ## Description
+    ${input.description}
+
+    ${input.licenseBadge}
+
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    * [License](#License)
+
+    ## Installation
+    ${input.install}
+
+    ## Usage
+    ${input.use}
+
+    ## Contributing
+    ${input.contribute}
+
+    ## Tests
+    ${input.test}
+
+    ## Questions
+    ${input.questions}
+    Any questions regarding this project can be directed to:
+    * Email: ${input.email}
+    * GitHub [${input.gitName}](https://github.com/${input.gitName})
+
+    ## License
+    Click on the badge below or near the top of this document, to find out more about the license for this product.
+    ${input.licenseBadge}
     `
 }
 
