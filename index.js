59972d3199be2692fbd6bc47f6da4c12c89172d5
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-24 20:46:09 +0000

    added fs functions

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import { questions } from "./userInput.js"
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(userInput) {
+    fs.mkdir("./generated-file", (err) => {if (err) throw err;})
+    fs.writeFile(`./generated-file/README.md`, generateReadme(userInput), (err) => err ? console.error(err) : console.log("Your README.md has been succesfully created in the generated-file folder."));
+}
 
 // TODO: Create a function to initialize app
 function init() {}
