d8c9afb6007d60d226243ec75ab94dfff2917461
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-24 23:46:14 +0000

    added generateBadge.js

---
+++ utils/badges.js
@@ -0,0 +1,20 @@
+function generateBadge(licenseType) {
+
+    if (licenseType === "MIT License") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }  else if (licenseType === "") {
+        return "";
+    }
+}
