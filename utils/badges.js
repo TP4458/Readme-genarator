eed1cdb0d1f0eb47a3ec7a8b6ed53a5695da5e6e
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-24 23:59:05 +0000

    added links to badges.js

--- utils/badges.js
+++ utils/badges.js
@@ -1,20 +1,16 @@
 function generateBadge(licenseType) {
 
     if (licenseType === "MIT License") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
-    }  else if (licenseType === "") {
-        return "";
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }  else if (licenseType === "Apache 2.0 License") {
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }  else if (licenseType === "GNU GPL v3.0") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    }  else if (licenseType === "Mozilla Public License 2.0") {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    }  else if (licenseType === "WTFPL") {
+        return "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+    }  else if (licenseType === "The Unlicense") {
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
     }
 }
